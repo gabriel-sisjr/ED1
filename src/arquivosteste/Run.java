7988aa470bb54a95aebaee19a94ce95c330fe0ee
Gabriel Santana <gabriel.sistemasjr@gmail.com>
2018-02-28 23:42:43 -0300

    subindo arquivos para o repo

---
+++ src/arquivosteste/Run.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arquivosteste;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Run {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        File arquivo = new File("Arquivo.txt");
+        Arquivo classeLer = new Arquivo();
+        ArrayList leitura = classeLer.retirarTabulacao(arquivo);
+        
+//        classeLer.escreverArquivo("bahiaViado",leitura);
+        classeLer.escreverArquivo("novoArquivo",leitura);
+        
+        // Imprimindo a frase
+//        leitura.forEach((frase) -> {System.out.println(frase);});
+    }
+}
